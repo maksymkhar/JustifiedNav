f8a2c5fd8ff19634d3c04e8339915861fb441a07
Max <maksymkharuk@iesebre.com>
2016-01-17 04:14:44 +0100

    Added Backstretch functionality

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by max on 17/01/16.
+ */
+
+$.backstretch([
+    "./assets/img/background_1.png"
+    , "./assets/img/background_2.jpg"
+    , "./assets/img/background_3.jpg"
+], {duration: 3000, fade: 750});
